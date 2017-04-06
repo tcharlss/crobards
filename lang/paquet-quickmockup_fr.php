a96bdd9fbf2a0e1bda9177ee391de8a25c7ebf3d
Charles Razack <charles@ldd.fr>
2017-04-06 18:20:25 +0200

    Mise à jour de la description

--- lang/paquet-quickmockup_fr.php
+++ lang/paquet-quickmockup_fr.php
@@ -5,7 +5,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'quickmockup_nom' => 'Quick Mockup',
-	'quickmockup_slogan' => 'Prototyper des maquettes vite fait.',
-	'quickmockup_description' => 'Un fork de QuickMockup de Jan Dittrich pour prototyper des interfaces de sites internet vite fait. Rendez vous sur la page spip.php?page=quickmockup',
+	'quickmockup_nom' => 'Crobards',
+	'quickmockup_slogan' => 'Prototyper des interfaces web vite fait.',
+	'quickmockup_description' => 'Rendez vous sur la page publique spip.php?page=quickmockup pour prototyper vite fait des interfaces web.
+
+		En phase de conception d\'une interface web, une étape consiste à réaliser des ébauches sur une feuille de papier pour définir dans les grandes lignes à quoi elle ressemblera, ce qu\'on appelle des «mockups», «filaires» ou encore «esquisses». Ce plugin les appelles des «crobards» et reprend le concept sous la forme d\'une petite application qui offre la possibilité de les enregister et de les partager.
+
+		D\'autres applications plus complètes existent déjà et permettent de manipuler plein d\'élements : des diagrammes, des polygonnes et touti quanti. Ce plugin ne cherche pas à tout faire, il se veut simple et se concentre sur les éléments propres aux interfaces web.
+
+		Il s\'agit d\'un fork de l\'application QuickMockup de Jan Dittrich',
 );
